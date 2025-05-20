2adedfce94d7a9c63380b7117c49f10e02d3700e
Srinivas <alsrinivas.346@gmail.com>
2025-05-20 14:45:15 +0530

    Initial Commit

---
+++ PortfolioWebsite/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
+
+
+export const Home=()=>{
+    return (
+        <div className="min-h-screen">
+
+            {/*ToggleTheme*/}
+
+            <ThemeToggle  />
+
+            {/*BackGroundEffect*/}
+            <StarBackground/>
+
+            {/*NavBar*/}
+
+            {/*MainContent*/}
+
+            {/*Footer*/}
+
+
+        </div>
+    )
+}
