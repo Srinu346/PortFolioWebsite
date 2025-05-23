dbeddda8c63c1ae017222a46ad27c6f0bf240a51
Srinivas <alsrinivas.346@gmail.com>
2025-05-23 17:46:17 +0530

    Second Commit

--- PortfolioWebsite/src/components/ProjectSection.jsx
+++ PortfolioWebsite/src/components/ProjectSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react"
 
 
 const projects = [
@@ -5,7 +6,7 @@ const projects = [
         id:1,
         title: "Movie App",
         description: "A movie app that allows users to search for movies and view details.",
-        image: "/projects/image.png",
+        image: "/projects/Screenshot 2025-05-22 171333.png",
         link: "",
         github: "",
         tags: ["React", "JavaScript", "CSS"],
@@ -14,7 +15,7 @@ const projects = [
         id:2,
         title: "Weather App",
         description: "A weather app that provides current weather information for any location.",
-        image:"\projects\image.png",
+        image:"/projects/image.png",
         link: "",
         github: "",
         tags: ["HTML", "JavaScript", "CSS"],
@@ -37,8 +38,39 @@ export const ProjectSection = () => {
                             <div className="h-48 overflow=hidden">
                                 <img src={project.image} alt={project.title} className="w-full h-full object-cover transistion-transform duration-500 group-hover:scale-110" />
                             </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-2">
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 font-medium border rounded-full bg-primary text-secondary-foreground">{tag}</span>
+                                    ))}
+                                </div>
+                            
+
+                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a className="text-foreground/80 hover:text-primary transistion-colors duration-300" href="{project.link}" target="_blank" rel="noopener noreferrer">
+                                        <ExternalLink size={20}/>
+                                    </a>
+
+                                    <a className="text-foreground/80 hover:text-primary transistion-colors duration-300" href="{project.github} " target="_blank" rel="noopener noreferrer">
+                                        <Github size={20}/>
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
                         </div>
                     ))}
+                </div>
+
+                <div className="text-center mt-12">
+                    <a className="cosmic-button w-fit flex itmes-center mx-auto gap-2" href="#" target="_blank">
+                        Check My GitHub <ArrowRight size={16} />
+                    </a>
                 </div>
             </div>
         </section>
--- PortfolioWebsite/src/pages/Home.jsx
+++ PortfolioWebsite/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { HeroSection } from "../components/HeroSection"
 import { AboutSection } from "../components/AboutSection"
 import { SkillSection } from "../components/SkillSection"
 import { ProjectSection } from "../components/ProjectSection"
-
+import { ContactSection } from "../components/ContactSection"
 
 export const Home=()=>{
     return (
@@ -27,6 +27,7 @@ export const Home=()=>{
                 <AboutSection />
                 <SkillSection />
                 <ProjectSection />
+                <ContactSection />
             </main>
 
             {/*Footer*/}
